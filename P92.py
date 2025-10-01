01ab5d659db816b292d4e5ab5f0c0e024deba54b
K.Balamurali <kolliparabalamurali06@gmail.com>
2025-10-01 13:07:16 +0530

    Dining Philosophers

--- P92.py
+++ P92.py
@@ -1,3 +1,4 @@
+#Dining Philosophers
 import threading,time,random
 def phil(i,l,r):
     for _ in range(2):
@@ -8,3 +9,4 @@ def phil(i,l,r):
 fs=[threading.Lock() for _ in range(5)]
 ths=[threading.Thread(target=phil,args=(i,fs[i],fs[(i+1)%5])) for i in range(5)]
 [t.start() for t in ths];[t.join() for t in ths]
+
