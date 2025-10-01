3b49fef443da0876f66aa5b408f2099a7c090669
K.Balamurali <kolliparabalamurali06@gmail.com>
2025-10-01 13:11:27 +0530

    Reader-Writer Problem with Semaphores

--- P100.py
+++ P100.py
@@ -1,3 +1,4 @@
+#Reader-Writer Problem with Semaphores
 import threading,time
 wsem=threading.Semaphore(1);rsem=threading.Semaphore(1);rc=0
 def reader(i):
@@ -11,3 +12,4 @@ def reader(i):
 def writer(i):wsem.acquire();print("Writer",i,"writing");time.sleep(.2);wsem.release()
 ths=[threading.Thread(target=reader,args=(i,)) for i in range(3)]+[threading.Thread(target=writer,args=(1,))]
 [t.start() for t in ths];[t.join() for t in ths]
+
