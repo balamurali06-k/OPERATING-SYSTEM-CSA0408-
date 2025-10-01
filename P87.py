c6a876a9204ae47883af49caaab20a18eaca2425
K.Balamurali <kolliparabalamurali06@gmail.com>
2025-10-01 13:03:39 +0530

    Non-Preemptive SJF

--- P87.py
+++ P87.py
@@ -1,3 +1,4 @@
+#Non-Preemptive SJF
 def simulate(ps):
     for p in ps:p['done']=0
     t=0;n=len(ps);c=0
@@ -7,3 +8,4 @@ def simulate(ps):
         cur=min(r,key=lambda x:x['burst']);cur['start']=t;cur['finish']=t+cur['burst'];t+=cur['burst'];cur['done']=1;c+=1
     for p in ps:print(p['pid'],p['finish']-p['arrival'],p['start']-p['arrival'])
 simulate([{'pid':1,'arrival':0,'burst':7},{'pid':2,'arrival':2,'burst':4}])
+
