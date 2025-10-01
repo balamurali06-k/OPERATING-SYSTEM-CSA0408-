ebea1be8e4af8af6f44f809a83ce3e9568c9fb3e
K.Balamurali <kolliparabalamurali06@gmail.com>
2025-10-01 13:08:39 +0530

    Two-Level Directory

--- P95.py
+++ P95.py
@@ -1,3 +1,4 @@
+#Two-Level Directory
 class FS:
     def __init__(s):s.u={}
     def mkuser(s,u):s.u.setdefault(u,{})
@@ -6,3 +7,4 @@ class FS:
     def read(s,u,f):return s.u.get(u,{}).get(f)
 fs=FS();fs.mkuser("a");fs.mkuser("b");fs.mkfile("a","x","ax");fs.mkfile("b","y","by")
 print(fs.list("a"));print(fs.read("a","x"))
+
